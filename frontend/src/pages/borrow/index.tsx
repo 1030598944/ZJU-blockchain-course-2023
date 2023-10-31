56826d2125a0d278e6fa75680baa789fcf6adb6e
张圣安 <1030598944@qq.com>
2023-10-31 23:27:35 +0800

    合约

--- frontend/src/pages/borrow/index.tsx
+++ frontend/src/pages/borrow/index.tsx
@@ -60,11 +60,11 @@ const LotteryPage = () => {
                     from: account
                 })
                 const updatedMyCars = ownerCars.map((carId: number) => new Car(carId));
-
+                setownercar(updatedMyCars);
                 let unborrowcar = await borrowYourCarContract.methods.getunborrowcar().call({
                     from: account
                 })
-                const u = ownerCars.map((carId: number) => new Car(carId));
+                const u = unborrowcar.map((carId: number) => new Car(carId));
                 setunborrowcar(u);
             } else {
                 alert('Contract not exists.')
@@ -203,41 +203,41 @@ const LotteryPage = () => {
     }
 
     const borrowcar = async () => {
-        // if(account === '') {
-        //     alert('未连接到钱包。')
-        //     return
-        // }
-        //
-        // if (borrowYourCarContract && myERC20Contract) {
-        //     try {
-        //         // 获取一辆新车
-        //         const owner =  await borrowYourCarContract.methods.getowner(inputborrowid).call()
-        //         const borrower =  await borrowYourCarContract.methods.getwhoborrowcar(inputborrowid).call()
-        //
-        //         if(owner === '0x0000000000000000000000000000000000000000'){
-        //
-        //             alert('该车辆不存在,请检查输入的车辆ID是否正确')
-        //         }
-        //         else if(owner===account){
-        //             alert('你不能借你自己的车')
-        //         }
-        //         else if(borrower === '0x0000000000000000000000000000000000000000'){
-        //             await myERC20Contract.methods.approve(borrowYourCarContract.options.address,inputtime).send({
-        //                 from: account
-        //             })
-        //             //借用车辆
-        //             await borrowYourCarContract.methods.borrowcar(inputborrowid, inputtime).send({
-        //                 from: account
-        //             })
-        //             alert('借用成功。')
-        //         }
-        //         else alert('车辆ID：' + inputborrowid + '\n车主是：' + owner + '\n借用者是：' + borrower)
-        //     } catch (error: any) {
-        //         alert(error.message)
-        //     }
-        // } else {
-        //     alert('合约不存在')
-        // }
+        if(account === '') {
+            alert('未连接到钱包。')
+            return
+        }
+
+        if (borrowYourCarContract && myERC20Contract) {
+            try {
+                // 获取一辆新车
+                const owner =  await borrowYourCarContract.methods.getowner(inputfindid).call()
+                const borrower =  await borrowYourCarContract.methods.getwhoborrowcar(inputfindid).call()
+
+                if(owner === '0x0000000000000000000000000000000000000000'){
+
+                    alert('该车辆不存在,请检查输入的车辆ID是否正确')
+                }
+                else if(owner===account){
+                    alert('你不能借自己的车')
+                }
+                else if(borrower === '0x0000000000000000000000000000000000000000'){
+                    await myERC20Contract.methods.approve(borrowYourCarContract.options.address,inputtime).send({
+                        from: account
+                    })
+                    //借用车辆
+                    await borrowYourCarContract.methods.borrowcar(inputborrowid, inputtime).send({
+                        from: account
+                    })
+                    alert('借用成功。')
+                }
+                else alert('车辆ID：' + inputfindid + '\n车主是：' + owner + '\n借用者是：' + borrower)
+            } catch (error: any) {
+                alert(error.message)
+            }
+        } else {
+            alert('合约不存在')
+        }
     }
 
 
@@ -252,18 +252,20 @@ const LotteryPage = () => {
                 {account === '' && <Button onClick={onClickConnectWallet}>连接钱包</Button>}
                 <div>当前用户：{account === '' ? '无用户连接' : account}</div>
                 <div>当前用户拥有浙大币数量：{account === '' ? 0 : accountBalance}</div>
-                {/*<Button onClick={newcar}>获取车辆</Button>*/}
-                {/*<div>*/}
-                {/*    <span>车辆ID：</span>*/}
-                {/*    <input type="number"style={{marginRight: '20px'}} value={inputfindid} onChange={e => setinputfindid(e.target.value)} />*/}
-                {/*    <Button style={{width: '200px'}} onClick={findid}>查询车辆</Button>*/}
-                {/*</div>*/}
-                {/*<div>*/}
-                {/*    <span>车辆ID：</span>*/}
-                {/*    <input type="number"style={{marginRight: '20px'}} value={inputborrowid} onChange={e => setinputborrowid(e.target.value)} />*/}
-                {/*    <input type="number"style={{marginRight: '20px'}} value={inputtime} onChange={e => setinputtime(e.target.value)} />*/}
-                {/*    <Button style={{width: '200px'}} onClick={borrowcar}>租借车辆</Button>*/}
-                {/*</div>*/}
+                <Button onClick={newcar}>获取车辆</Button>
+                <div>
+                    <span>车辆ID：</span>
+                    <input type="number"style={{marginRight: '20px'}} value={inputfindid} onChange={e => setinputfindid(e.target.value)} />
+                    <Button style={{width: '200px'}} onClick={findid}>查询车辆</Button>
+                    <div>{inputfindid}</div>
+                </div>
+                <div>
+                    <span>车辆ID：</span>
+                    <input type="number"style={{marginRight: '20px'}} value={inputborrowid} onChange={e => setinputborrowid(e.target.value)} />
+                    <input type="number"style={{marginRight: '20px'}} value={inputtime} onChange={e => setinputtime(e.target.value)} />
+                    <Button style={{width: '200px'}} onClick={borrowcar}>租借车辆</Button>
+                    <div>{inputfindid}</div>
+                </div>
                 <div>我的车辆</div>
                 <ul>
                     {
@@ -275,16 +277,16 @@ const LotteryPage = () => {
                         ))
                     }
                 </ul>
-                {/*<div>空闲的车辆</div>*/}
-                {/*<ul>*/}
-                {/*    {unborrowcar.map((car) => (*/}
-                {/*        <li key={car.tokenId}>*/}
-                {/*            <span>车辆ID：{car.tokenId}</span>*/}
-                {/*            <br></br>*/}
-                {/*            <ImageComponent tokenId={car.tokenId} />*/}
-                {/*        </li>*/}
-                {/*    ))}*/}
-                {/*</ul>*/}
+                <div>空闲的车辆</div>
+                <ul>
+                    {unborrowcar.map((car) => (
+                        <li key={car.tokenId}>
+                            <span>车辆ID：{car.tokenId}</span>
+                            <br></br>
+                            <ImageComponent tokenId={car.tokenId} />
+                        </li>
+                    ))}
+                </ul>
 `
             </div>
         </div>
